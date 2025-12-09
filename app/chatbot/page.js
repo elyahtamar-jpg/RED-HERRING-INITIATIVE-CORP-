3b543ffffedda39fe061dff6fbd3c58d82e505c7
elyahtamar-jpg <elyahtamar@gmail.com>
2025-12-08 23:33:26 -0500

    Update page.js

--- app/chatbot/page.js
+++ app/chatbot/page.js
@@ -4,7 +4,7 @@ import Chatbot from "../../components/Chatbot";
 
 export default function ChatbotPage() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px", maxWidth: "700px", margin: "0 auto" }}>
       <Chatbot />
     </div>
   );

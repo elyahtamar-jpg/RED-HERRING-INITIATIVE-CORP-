ee35f7737514f7ca5a21c0396bed3bba58d43132
elyahtamar-jpg <elyahtamar@gmail.com>
2026-02-01 12:12:33 -0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: "Arial, sans-serif" }}>
-      <h1>Red Herring Initiative Corp</h1>
-      <p>Exposing Injustice. Empowering Communities. Delivering Accountability.</p>
-      <a href="/complaint">File a Complaint</a>
-    </div>
-  );
-}
